382766bf998bf025e98ac04f0c6f682234cdbd5c
cnbattle <liqiai@live.com>
2018-07-10 09:36:03 +0800

    Update zshrc.zsh

--- zshrc.zsh
+++ zshrc.zsh
@@ -71,7 +71,7 @@ antigen bundle willghatch/zsh-cdr
 # antigen bundle zsh-users/zaw
 
 # Load the theme.
-antigen theme robbyrussell
+# antigen theme robbyrussell
 
 # check login shell
 if [[ -o login ]]; then
